6caa27e7df225aef4a52dcb841e4b6365574ab8a
Talbe <talbarerez@gmail.com>
2024-10-13 15:55:32 +0300

    Path Generation - Refactored code. - Added proper timestamps support.

--- RangedEntitiesFinder.py
+++ RangedEntitiesFinder.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from geopy import distance as gpy_distance
 import pandas as pd
 import math
@@ -6,22 +8,13 @@ from datetime import datetime, timedelta
 import random as rand
 
 class ranged_entity_finder():
-    total_df = pd.DataFrame # DataFrame that contains data about the path of entities including a target.
-    sus_df = pd.DataFrame   # DataFrame that contains data about the path of the target.
-
-    def __init__(self, total: pd.DataFrame, sus: pd.DataFrame):
-        if total.shape[1] != 5 or sus.shape[1] != 5:
-            raise ValueError("Table dimensions are incorrect!")
-
-        self.total_df = total
-        self.sus_df = sus
-
     '''
     Calculates the distance between two gives given points
     on Earth. Each points consists of three values,
     Latitude, Longitude, and height above the surface (in km).
     '''
-    def _calc_distance(self, point_origin: tuple, point_destination: tuple) -> float:
+    @staticmethod
+    def _calc_distance(point_origin: tuple, point_destination: tuple) -> float:
         if point_origin == point_destination: return 0
 
         # Saving the latitude and longitude coordinates separately.
@@ -30,58 +23,74 @@ class ranged_entity_finder():
 
         # Calculting the distance between the latitude and longitude coordinates.
         horiz_distance = gpy_distance.geodesic(
-            horiz_coords_origin, 
+            horiz_coords_origin,
             horiz_coords_destination).km
 
         # Calculating the height difference between the points.
         vert_distance = point_destination[2] - point_origin[2]
 
-        # Applying pythagoras theorm to calculate the distance between the two points.
+        # Applying pythagoras theorem to calculate the distance between the two points.
         return math.sqrt(horiz_distance**2 + vert_distance**2)
+
+    '''
+    Rounds the coordinates in the tuple to 3 places after decimal point.
+    '''
+    @staticmethod
+    def _round_coordinates(point_to_round: tuple, places_after_decimal: int = 3):
+        if places_after_decimal <= 0: places_after_decimal = 3
+
+        return (round(point_to_round[0], places_after_decimal),
+                round(point_to_round[1], places_after_decimal),
+                round(point_to_round[2], places_after_decimal))
 
 
     '''
     Locates the closest entities within a defined distance from the **sus**picious target.
     The distance is measured in km.
+    
+    total_df:   DataFrame that contains data about the path of entities including a target.
+    sus_df:     DataFrame that contains data about the path of the target.
     '''
-    def locate_closest_entities_to_target(self, distance_from_target = 1000.0) -> None:
+    @staticmethod
+    def locate_closest_entities_to_target(total_df: pd.DataFrame, sus_df: pd.DataFrame, distance_from_target = 1000.0) -> bool:
         # Checking if distance is valid.
         if distance_from_target <= 0: 
             print("Max distance can't be smaller or equal to 0!")
-            return
+            return False
 
-        # Finding the target Id to drop from total_df for easier navigation in the data.
-        entity_id_to_filter = list(self.sus_df["id"])[0]
+        # Finding the target ID to drop from total_df for easier navigation in the data.
+        entity_id_to_filter = list(sus_df["id"])[0]
 
         # Validating sus table.
-        if all(self.sus_df["id"] != entity_id_to_filter):
+        if all(sus_df["id"] != entity_id_to_filter):
             print("Sus table is invalid!")
-            return
+            return False
 
         # Removing (dropping) the data about the target from total_df to make working with the data easier.
-        # The target might had been dropped since a previous user input during runtime.
-        self.total_df = self.total_df[self.total_df["id"] != entity_id_to_filter]
-
+        # The target might have been dropped since a previous user input during runtime.
+        total_df = total_df[total_df["id"] != entity_id_to_filter]
         
-        # Saving occurences of entities crossing paths with the target without timestamp relation.
+        # Saving occurrences of entities crossing paths with the target without timestamp relation.
         # Grouping according to timestamps is done later.
-        path_crosses = self.total_df.merge(
-            self.sus_df[["lat", "long", "height"]],
+        path_crosses = total_df.merge(
+            sus_df[["lat", "long", "height"]],
             on=["lat", "long", "height"])
+
+        # Checking if any cross paths exist.
+        if path_crosses.empty:
+            print("No entities crossed paths with target!")
+            return False
 
         # Grouping the path crosses according to timestamps (by rising order), latitude, longitude, and height.
         path_crosses_grouped = path_crosses.groupby(by=["ts", "lat", "long", "height"])[["id"]].apply(dict).dropna()
         
         # Storing the coordinates (as points) as a Series of dictionary of tuples, stored in sus DataFrame.
-        path_points_sus = self.sus_df.groupby(by=["ts"])[["lat", "long", "height"]].apply(dict)
-        print("total:", self.total_df)
-        print("sus:", self.sus_df)
+        path_points_sus = sus_df.groupby(by=["ts"])[["lat", "long", "height"]].apply(dict)
 
         print("Format of coordinates: (latitude [deg], longitude [deg], height [km])\n")
 
         # Iterating over the path of the target entity.
         for current_ts in path_points_sus.keys():
-
             if current_ts not in path_crosses_grouped.index: continue
 
             # The current group of entities (except target) in the current timestamp.
@@ -93,18 +102,23 @@ class ranged_entity_finder():
             # val.values[0] gets the needed value.
             sus_location = tuple(float(val.values[0]) for val in path_points_sus[current_ts].values())
 
-            # Previou timestamp is used for checking if the current timestamp was printed already.
+            # Previous timestamp is used for checking if the current timestamp was printed already.
             prev_ts = math.nan
 
             # Iterating over the group of entities which belong to the current timestamp.
             for current_entities in ts_group.items():
-                distance_calculated = self._calc_distance(sus_location, current_entities[0])
+                distance_calculated = ranged_entity_finder._calc_distance(sus_location, current_entities[0])
 
                 if distance_calculated >= distance_from_target: continue
 
                 if prev_ts != current_ts:
                     print(f"---------- Timestamp {current_ts} ----------")
                     prev_ts = current_ts
+
+                # Rounding the coordinate values of the entities (both that are close to target and of target itself).
+                # Used purely for printing.
+                entities_location_rounded = ranged_entity_finder._round_coordinates(current_entities[0])
+                sus_location_rounded = ranged_entity_finder._round_coordinates(sus_location)
 
                 print(f"Entity(ies):", end=' ')
 
@@ -116,9 +130,13 @@ class ranged_entity_finder():
                 # The [0] is used to read only the Ids of the entities, without the idexes of the series.
                 print(*list(current_entities[1].values())[0], sep=", ", end=' ')
                 print(f"is/are close to target ({round(distance_calculated, 3)} km)!\n" 
-                    + f"Entity(ies) coordinates:\t{current_entities[0]}\nTarget coordinates:\t\t{sus_location}\n")
+                    + f"Entity(ies) coordinates:\t{entities_location_rounded}\nTarget coordinates:\t\t{sus_location_rounded}\n")
 
+        return True
 
+    '''
+    Generates a path of an entity that doesn't cross paths with sus.
+    '''
     @staticmethod
     def _generate_entity_path(entity_num: int, num_tracks: int) -> pd.DataFrame:
         entity_idx = entity_num - 1
@@ -140,15 +158,105 @@ class ranged_entity_finder():
 
         return pd.DataFrame(data)
 
+
+    '''
+    Generates a path of an entity that crosses paths with a target entity.
+    '''
     @staticmethod
-    def _generate_entity_with_target_cross_path(entity_num: int, sus_num: int, time_of_sus: datetime, num_tracks: int) -> pd.DataFrame:
+    def _generate_entity_with_target_cross_path(entity_num: int, sus_num: int, num_tracks: int) -> pd.DataFrame:
+        # Calculating indexes of entities for easier data access.
         entity_idx = entity_num - 1
-        sus_idx = sus_num - 1
+        #sus_idx = sus_num - 1
 
-        sus_at_time = sus_path[sus_path["id"] == sus_num and sus_path["ts"] == time_of_sus]
+        # Selecting a random timestamp (ts_of_cross_path) from an index (ts_idx_of_cross_path)
+        # to be a time when a cross path occurs.
+        ts_idx_of_cross_path = rand.randint(0, num_tracks - 1)
+        ts_of_cross_path = timestamps[ts_idx_of_cross_path]
 
-        sus_location_at_time = (sus_at_time["lat"], sus_at_time["long"], sus_at_time["height"])
+        # The row in the DataFrame that contains that data about the entity (such as location) at the time when crossing paths.
+        sus_data_at_ts = sus_path[(sus_path["id"] == sus_num) & (sus_path["ts"] == ts_of_cross_path)]
 
+        # Getting the index of sus from the DataFrame.
+        sus_idx_in_df = sus_data_at_ts.index[0]
+
+        # .loc is used to make sure the tuple contains only np.float values and not Pandas Series.
+        cross_path_location = (sus_data_at_ts.loc[sus_idx_in_df, "lat"],
+                               sus_data_at_ts.loc[sus_idx_in_df, "long"],
+                               sus_data_at_ts.loc[sus_idx_in_df, "height"])
+
+        cross_path_idx_of_entity = rand.randint(0, num_tracks - 1)
+
+        entity_starting_location = (starting_positions[entity_idx, 0],
+                                    starting_positions[entity_idx, 1],
+                                    starting_heights[entity_idx])
+
+        '''
+        Calculating the lat, long and height increments required to reach the cross path location at the timestamp
+        specified in relation to the beginning.
+        Example: an entity starts at the 1st timestamp, and the cross paths is at the 5th.
+                 Starting location of entity: (12, 18, 30)
+                 Cross paths location:        (15, 6, 29.1)
+                 Between the 1st timestamp and 5th there are 3 timestamps, 2nd, 3rd, and 4th timestamps.
+                 Because of this, there are 3 increments between the starting position and cross paths.
+                 Meaning that the increments will be:
+                 1. lat:    |12 - 15| * 1 = 3
+                 2. long:   |18 - 6| * (-1) = -4
+                 3. height: |30 - 29.1| * (-1) = -0.3
+                 Note that if the starting location value of the current axis is bigger than
+                 the value in the same axis of the cross paths location, the increment will be negative
+                 (absolute value multiplied by -1).
+                 Using the entity index minus -1 gives the amount of timestamps in between the start and end. 
+        '''
+        #region Increments Calculation
+        # Calculating the coordinate differences on each axis.
+        lat_new_increment = math.fabs(entity_starting_location[0] - cross_path_location[0])
+
+        long_new_increment = math.fabs(entity_starting_location[1] - cross_path_location[1])
+
+        height_new_increment = math.fabs(entity_starting_location[2] - cross_path_location[2])
+
+        # Calculating the amount of timestamps between the beginning and cross paths.
+        ts_between_amount = cross_path_idx_of_entity - 1
+
+        # Calculating the increments given that the amount of timestamps between beginning and
+        # cross paths is larger than zero. Otherwise, the increments are straight from beginning to end.
+        if ts_between_amount > 0:
+            lat_new_increment /= ts_between_amount
+            long_new_increment /= ts_between_amount
+            height_new_increment /= ts_between_amount
+
+        # Verifying the sign of each increment is correct.
+        if entity_starting_location[0] > cross_path_location[0]: lat_new_increment *= -1
+
+        if entity_starting_location[1] > cross_path_location[1]: long_new_increment *= -1
+
+        if entity_starting_location[2] > cross_path_location[2]: height_new_increment *= -1
+        #endregion
+
+        # Generating the rest of the positions of timestamps that connect between the beginning to the cross paths,
+        # and all the way to the end of the path of the entity.
+        #for current_location_multiplier in range(2, num_tracks):
+        entity_path = {
+            "id": np.repeat(entity_num, num_tracks),
+            "ts": timestamps,
+            "lat":
+                starting_positions[entity_idx, 0] + lat_new_increment *
+                np.arange(num_tracks),
+            "long":
+                starting_positions[entity_idx, 1] + long_new_increment *
+                np.arange(num_tracks),
+            "height":
+                starting_heights[entity_idx] + height_new_increment *
+                np.arange(num_tracks)
+        }
+
+        return pd.DataFrame(entity_path)
+
+
+    '''
+    Generates paths of entities based on the amount of tracks (timestamps).
+    Some entities cross paths with a suspicious target (sus).
+    '''
     @staticmethod
     def generate_entities_paths(num_entities: int = 5, num_tracks: int = 10, rand_sus_num: bool = False):
         # Making variables global to reduce amount of function arguments of those being called from this function.
@@ -158,69 +266,42 @@ class ranged_entity_finder():
         timestamps = [datetime.now() + timedelta(seconds=i) for i in range(num_tracks)]
 
         # Initialize starting positions and linear increments
+        # The defined increments (lat_increments, long_increments, height_increments)
+        # are the default increments when an entity does not cross paths with the target (sus).
         starting_positions  = np.random.uniform(low=-90.0, high=90.0, size=(num_entities, 2))   # Starting lat and long
-        starting_heights    = np.random.uniform(low=0.0, high=10000.0, size=num_entities)       # Starting height
+        starting_heights    = np.random.uniform(low=0.0, high=100.0, size=num_entities)         # Starting height
         lat_increments      = np.random.uniform(low=-0.1, high=0.1, size=num_entities)          # Lat change per step
         long_increments     = np.random.uniform(low=-0.1, high=0.1, size=num_entities)          # Long change per step
         height_increments   = np.random.uniform(low=-10.0, high=10.0, size=num_entities)        # Height change per step
 
+        # Giving an initial value to the target entity, as it might not be randomized.
         sus_num = 1
 
+        # Randomizing the sus target.
         if rand_sus_num and num_entities > 1:
             sus_num = rand.randint(2, num_entities)
 
+        # Generating the path of the target entity.
         sus_path = ranged_entity_finder._generate_entity_path(sus_num, num_tracks)
 
+        # Initializing a DataFrame that stores all the data of the entities (including the target).
+        all_entities = pd.DataFrame()
+
         for current_entity_num in range(1, num_entities):
+            if current_entity_num == sus_num:
+                # Adding the sus target to the DataFrame with all entities.
+                all_entities = pd.concat([all_entities, sus_path]).reset_index(drop=True)
+                continue
+
+            # Making random entities cross paths with the target.
             cross_paths_with_target = bool(rand.randint(0, 1))
 
             if cross_paths_with_target:
-                ranged_entity_finder._generate_entity_with_target_cross_path(current_entity_num, sus_num, ) #todo: continue here
+                new_entity_data = ranged_entity_finder._generate_entity_with_target_cross_path(current_entity_num, sus_num, num_tracks)
 
-    @staticmethod
-    def generate_data(num_entities: int = 5, num_tracks: int = 10, rand_num_tracks: bool = False) -> tuple[pd.DataFrame, pd.DataFrame]:
-        if rand_num_tracks:
-            num_tracks = rand.randint(int(num_tracks / 2), num_tracks)
+            else: new_entity_data = ranged_entity_finder._generate_entity_path(current_entity_num, num_tracks)
 
-        # Generate a single set of timestamps
-        timestamps = [datetime.now() + timedelta(seconds=i) for i in range(num_tracks)]
+            all_entities = pd.concat([all_entities, new_entity_data]).reset_index(drop=True)
 
-        # Initialize starting positions and linear increments
-        start_positions     = np.random.uniform(low=-90.0, high=90.0, size=(num_entities, 2))   # Starting lat and long
-        height_starts       = np.random.uniform(low=0.0, high=10000.0, size=num_entities)       # Starting height
-        lat_increment       = np.random.uniform(low=-0.1, high=0.1, size=num_entities)          # Lat change per step
-        long_increment      = np.random.uniform(low=-0.1, high=0.1, size=num_entities)          # Long change per step
-        height_increment    = np.random.uniform(low=-10.0, high=10.0, size=num_entities)        # Height change per step
-
-        # Generate data
-        data = {
-            "id": np.repeat(np.arange(1, num_entities + 1), num_tracks),
-            "ts": np.tile(timestamps, num_entities),
-            "lat": np.concatenate([
-                start_positions[i, 0] + lat_increment[i] * 
-                np.arange(num_tracks) for i in range(num_entities)
-            ]),
-            "long": np.concatenate([
-                start_positions[i, 1] + long_increment[i] * 
-                np.arange(num_tracks) for i in range(num_entities)
-            ]),
-            "height": np.concatenate([
-                height_starts[i] + height_increment[i] * 
-                np.arange(num_tracks) for i in range(num_entities)
-            ])
-        }
-
-        data = pd.DataFrame(data)
-
-        entity_ids = list(data.groupby(by="id").groups)
-
-        entity_idx_to_be_target = rand.randint(0, len(entity_ids) - 1)
-        
-        sus_path = data[data["id"] == entity_ids[entity_idx_to_be_target]]
-
-        entities_path = data[data["id"] != entity_ids[entity_idx_to_be_target]]
-        print("data:", data.groupby(["lat", "long", "height"]).apply(pd.DataFrame), "\n\n\n\n")
-
-        #print(sus_path)
-        # Create DataFrame and return
-        return (entities_path, sus_path)
+        # Returning DataFrames in a tuple.
+        return (all_entities, sus_path)
--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import pandas as pd
-from RangedEntitiesFinder import ranged_entity_finder as rEF
+from RangedEntitiesFinder import ranged_entity_finder as rEF, ranged_entity_finder
 import openpyxl
 
 if __name__ == "__main__":
     # Reading data from Excel into DataFrames.
-    total_df = pd.read_excel(r"data/dataTables.xlsx", "total", converters={"id": str})
-    sus_df = pd.read_excel(r"data/dataTables.xlsx", "sus", converters={"id": str})
+    #total_df = pd.read_excel(r"data/dataTables.xlsx", "total", converters={"id": str})
+    #sus_df = pd.read_excel(r"data/dataTables.xlsx", "sus", converters={"id": str})
 
-    # calcObj = rEF(total_df, sus_df)
+    num_entities = 5
+    num_tracks = 10
 
-    #(total_df, sus_df) = rEF.generate_data(5, 10, False)
+    (total_df, sus_df) = rEF.generate_entities_paths(num_entities, num_tracks, True)
 
-    calcObj = rEF(total_df, sus_df)
+    print("--- Enter max distance from target in km (or enter 's' to quit, or 'r' to generate new data) ---")
 
-    #print("test:", list(sus_df["id"])[0])
-
-    rEF.generate_entities_paths()
-
-    max_distance_from_target = input("Enter max distance from target in km (or enter 's' to stop the program): ")
+    max_distance_from_target = None
 
     while max_distance_from_target != 's':
-        if max_distance_from_target == 's': break
+        max_distance_from_target = input("\nYour input (a number, 's', or 'r'): ")
+
+        if max_distance_from_target == 'r':
+            print("Generating new data...")
+            (total_df, sus_df) = rEF.generate_entities_paths(num_entities, num_tracks, True)
+            continue
 
         try:
-            calcObj.locate_closest_entities_to_target(float(max_distance_from_target))
+            res = ranged_entity_finder.locate_closest_entities_to_target(total_df, sus_df, float(max_distance_from_target))
 
         except ValueError as ex:
             print(ex)
 
-        max_distance_from_target = input("\nEnter max distance from target in km (or enter 's' to stop the program): ")
+        if not res:
+            print("Data invalid or no path crosses were found! Generating new data...")
+            (total_df, sus_df) = rEF.generate_entities_paths(num_entities, num_tracks, True)
